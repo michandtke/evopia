9cc3b698a122d253c6fcb6dd6b565ca7ff80840a
Michael Wandtke <michael.wandtke@hey.com>
2021-10-16 16:00:07 +0200

    Add a little bit of styling for the event details page

--- lib/event_details.dart
+++ lib/event_details.dart
@@ -19,34 +19,80 @@ class _EventDetailsState extends State<EventDetails> {
         appBar: AppBar(
           title: const Text("Event details"),
         ),
-        body: body()
-    );
-
+        body: body());
   }
 
   Widget body() {
     return Column(children: [
-      nameWidget(),
+      topContent(),
       descriptionWidget(),
-      dateAndTime(),
-      tags(),
     ]);
   }
 
+  Widget topContent() {
+    return Stack(
+      children: <Widget>[
+        Container(
+            padding: const EdgeInsets.only(left: 10.0),
+            height: MediaQuery.of(context).size.height * 0.5,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("drive-steering-wheel.jpg"),
+                fit: BoxFit.cover,
+              ),
+            )),
+        Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            padding: const EdgeInsets.all(40.0),
+            width: MediaQuery.of(context).size.width,
+            decoration:
+                const BoxDecoration(color: Color.fromRGBO(58, 66, 86, .9)),
+            child: Column(
+              children: [
+                nameWidget(),
+                dateAndTime(),
+                const Expanded(
+                  child: Text(""),
+                ),
+                tags()
+              ],
+            )),
+        Positioned(
+          left: 8.0,
+          top: 60.0,
+          child: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.arrow_back, color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }
+
   Widget nameWidget() {
-    return Text(widget.event.name);
+    return Text(
+      widget.event.name,
+      style: const TextStyle(color: Colors.white, fontSize: 45.0),
+    );
   }
 
   Widget descriptionWidget() {
-    return Text(widget.event.description);
+    return Text(widget.event.description,
+        style: const TextStyle(fontSize: 24.0));
   }
 
   Widget dateAndTime() {
-    return Text("${widget.event.date} - ${widget.event.time}");
+    return Text(
+      "${widget.event.date} - ${widget.event.time}",
+      style: const TextStyle(color: Colors.white, fontSize: 20.0),
+    );
   }
 
   Widget tags() {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
       children: widget.event.tags.map((t) => TagEntry(name: t)).toList(),
     );
   }
