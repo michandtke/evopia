d0aa3541f36e90cde0574e0c94a4084399507c08
michandtke <Michael.Wandtke@hey.com>
2022-05-07 11:13:02 +0200

    Adjust config to make environment variable fallback of env file
    The env file will be used locally, the variable will be used in CI. Not sure if it's the best. But it should work.

--- lib/simple_config.dart
+++ lib/simple_config.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class SimpleConfig {
-  static String baseUrl = dotenv.env['EVOPIA_BASE_URL'] ?? "";
+  static String baseUrl = dotenv.get('EVOPIA_BASE_URL',
+      fallback: const String.fromEnvironment('EVOPIA_BASE_URL'));
 }
